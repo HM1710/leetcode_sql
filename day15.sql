07fe762f80958a33a624e770c1d3cf40669c11ee
HM1710 <hgmehta95@gmail.com>
2021-02-09 17:41:06 -0500

    Update day15.sql

--- day15.sql
+++ day15.sql
@@ -1,5 +1,5 @@
 -- https://platform.stratascratch.com/coding-question?id=10299&python=
-
+--01
 SELECT e.id, e.first_name, e.last_name, e.department_id, f.sal AS max
 FROM ms_employee_salary e
     JOIN(select id, MAX(salary) AS sal
@@ -7,3 +7,9 @@ FROM ms_employee_salary e
         GROUP BY id) f
     ON e.id=f.id AND e.salary=f.sal
 ORDER BY e.id;
+
+--02
+SELECT id, first_name, last_name, department_id, MAX(salary) AS max
+FROM ms_employee_salary
+GROUP BY id, first_name, last_name, department_id
+ORDER BY id;
