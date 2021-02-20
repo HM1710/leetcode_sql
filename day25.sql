6da62f97db42dc3bf5ed2ea6c9515a2c8bc07448
Harsh Mehta <hgmehta95@gmail.com>
2021-02-19 23:37:22 -0500

    Update day25.sql

--- day25.sql
+++ day25.sql
@@ -1,5 +1,5 @@
 -- https://platform.stratascratch.com/coding-question?id=9897&python=
-
+-- 01
 WITH cte AS
 ( SELECT department, first_name, salary, RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS number
   FROM employee
@@ -8,3 +8,10 @@ WITH cte AS
 SELECT cte.department, cte.first_name, cte.salary
 FROM cte
 WHERE cte.number=1;
+
+--02
+SELECT department, first_name, salary
+FROM employee
+WHERE (department, salary) IN (SELECT department, MAX(salary) AS sal
+                               FROM employee
+                               GROUP BY department);
